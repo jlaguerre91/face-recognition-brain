dc879ea64359ee0badf4118da595431bc7508c3b
jlaguerre91 <jlaguerre91@gmail.com>
2022-07-24 21:14:50 -0400

    Updates

---
+++ precache-manifest.dbbea4311c9d7ff39530c51bac9d2f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567137ebf27704939b5274425c652b99",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "fee468c270a3e9dbc10d",
+    "url": "/face-recognition-brain/static/css/2.85c36af2.chunk.css"
+  },
+  {
+    "revision": "596209a96afe8e92d318",
+    "url": "/face-recognition-brain/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "fee468c270a3e9dbc10d",
+    "url": "/face-recognition-brain/static/js/2.d748cf9f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/face-recognition-brain/static/js/2.d748cf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596209a96afe8e92d318",
+    "url": "/face-recognition-brain/static/js/main.faca7a1c.chunk.js"
+  },
+  {
+    "revision": "33be454f861c9d52b30c",
+    "url": "/face-recognition-brain/static/js/runtime-main.d41efdb7.js"
+  }
+]);
